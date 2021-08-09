650f99def4cedd42642edefec70d002926c95dcb
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-08-09 00:21:02 -0400

    navbar, css, added pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+export default function App({ Component, props }) {
+	return (
+		<>
+			<header>
+				<h1>./xmagee</h1>
+				<p>my blog</p>
+			</header>
+
+			<nav style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+				{[
+					{ title: 'Home', href: '/'}, 
+					{ title: 'Blogs', href: '/blogs'}, 
+					{ title: 'Projects', href: '/projects'}, 
+					{ title: 'Github', href: 'https://github.com/xmagee'}, 
+					{ title: 'Resume', href: 'https://alexmagee.com'}, 
+				].map((link, linkIndex) => (
+					<>
+						<Link key={linkIndex} href={link.href}>
+							<a style={{marginLeft: 5, marginRight: 5}}>{link.title}</a>
+						</Link>
+						{linkIndex !== 4 && ('|')}
+					</>
+				))}
+			</nav>
+
+			<Component {...props} />
+		</>
+	)
 }

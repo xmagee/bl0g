f7d8cc198b26514cc437ce96c86d39491bdbcb54
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-08-21 17:09:55 -0400

    Converted blogs to MD, using react-markdown

--- pages/blog/2.js
+++ pages/blog/2.js
@@ -1,22 +1,20 @@
-import { BlogDatebar } from '../../components'
+import ReactMarkdown from 'react-markdown'
 
 export default function Blog2() {
     return (
-        <>
-            <BlogDatebar title='blog #2' date='Aug 13 2021' />
-            
-            <p>
-                Made some changes, I think this is good now. I initially used
-                css-in-js because I just wanted to throw this thing together, 
-                but i've put it all into .css files since I still think its easier
-                to manage like that. In the future I would like to have a better 
-                system of storing the blogs, but for now this is fine. 
-            </p>
+        <ReactMarkdown 
+                parserOptions={{ commonmark: true }} 
+                children={`
+## blog #2 | Aug 13 2021
 
-            <p>
-                Now, lets see if I can actually maintain this and post new things here, and 
-                not just abandon another blog, and eventually replace it :).
-            </p>
-        </>
+Made some changes, I think this is good now. I initially used
+css-in-js because I just wanted to throw this thing together, 
+but i've put it all into .css files since I still think its easier
+to manage like that. In the future I would like to have a better 
+system of storing the blogs, but for now this is fine. 
+
+Now, lets see if I can actually maintain this and post new things here, and 
+not just abandon another blog, and eventually replace it :). 
+            `}  />
     )
 }

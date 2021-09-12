0e1a7a94bf644931938b5d1e01812644376ad0d9
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-09-12 17:16:56 -0400

    Added spotify embed to home page

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,36 @@ export default function Home() {
 
 			<p>
 				Hi! I am a software developer from Pennsylvania, with a B.S. in Computer
-				Information Systems & Technology, with a passion for writing code and finding 
-				new flavors of La Croix! This site is my personal blog, but feel free to check out my 
-				resume and some projects that I am working on / have worked on. Cya!
+				Information Systems & Technology.  I enjoy all kinds of projects but 
+				professionally, I would call myself a 'web developer'; I typically lean on 
+				ReactJS to make front-ends, and ExpressJS to build web api's. 
 			</p>
+
+			<p>
+				I left some projects on this site on the <a href='/projects'>/projects</a> page, but to get the latest
+				and greatest check out my <a href='https://github.com/xmagee'>github profile</a>. 
+			</p>
+
+			<hr className='hr-with-margin' />			
+
+			<div className='spotify-section'>
+				<div className='spotify-weekly-title'>
+					<h3>
+						<u>Weekly Spotify Playlist</u>
+					</h3>
+
+					<i>My fav. songs this week</i>
+				</div>
+				
+				<div className='spotify-weekly-container'>
+					<iframe src='https://open.spotify.com/embed/playlist/3ZafZWKVyWRvWf8cuA00Ch' 
+						width='450' 
+						height='300' 
+						frameborder='0' 
+						allowtransparency='true' 
+						allow='encrypted-media' />
+				</div>
+			</div>
 		</>
 	)
 }

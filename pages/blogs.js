1f0012c1a761f843f58d0ed7f8ac43c54b69cb91
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-09-12 16:18:34 -0400

    moved arrays out of the way

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,6 +1,16 @@
 import Link from 'next/link'
 
 export default function Blogs() {
+    const blog_links = [
+        { href: '/blog/7', title: '🌐 checkin\' out micro api stuff' }, 
+        { href: '/blog/6', title: 'another month, another resume' },
+        { href: '/blog/5', title: 'elementaryOS 6 is very cool' }, 
+        { href: '/blog/4', title: 'this blog, now using markdown! 👋😲👋 💯💯💯' },
+        { href: '/blog/3', title: 'documentation, javascript, rust, and more ?' },
+        { href: '/blog/2', title: 'blog improvements, maybe done?' },
+        { href: '/blog/1', title: 'testing' }, 
+    ]
+    
     return (
         <>
             <span className='page-title-subtitle-container'>
@@ -14,15 +24,7 @@ export default function Blogs() {
             </span>
 
             <ul className='blog-ul'>
-                {[
-                    { href: '/blog/7', title: '🌐 checkin\' out micro api stuff' }, 
-                    { href: '/blog/6', title: 'another month, another resume' },
-                    { href: '/blog/5', title: 'elementaryOS 6 is very cool' }, 
-                    { href: '/blog/4', title: 'this blog, now using markdown! 👋😲👋 💯💯💯' },
-                    { href: '/blog/3', title: 'documentation, javascript, rust, and more ?' },
-                    { href: '/blog/2', title: 'blog improvements, maybe done?' },
-                    { href: '/blog/1', title: 'testing' }, 
-                ].map((link, linkIndex) => (
+                {blog_links.map((link, linkIndex) => (
                     <li key={linkIndex}>
                         <Link href={link.href}>
                             <a>{link.title}</a>

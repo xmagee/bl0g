a33610e11b04c1b158806926a5529bea5d9d0902
Alex Magee <alex@magee.us>
2022-03-30 23:33:13 -0400

    Added next-sitemamp package

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['images1.us-east-1.linodeobjects.com']
-  }
+  },
+  siteUrl: process.env.SITE_URL || 'https://xmagee.com',
+  generateRobotsTxt: true,
 }
